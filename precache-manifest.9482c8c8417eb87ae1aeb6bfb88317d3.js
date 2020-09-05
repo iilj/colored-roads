148544425113617258aa822343315b7a2679c87f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-05 10:29:02 +0000

    Deploy iilj/colored-roads to github.com/iilj/colored-roads.git:gh-pages

---
+++ precache-manifest.9482c8c8417eb87ae1aeb6bfb88317d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421e90471153ac54e5d3066af9ec2c06",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7d745084a73b35ead744",
+    "url": "./static/css/2.9b568130.chunk.css"
+  },
+  {
+    "revision": "7686073839be7166df5e",
+    "url": "./static/css/main.ca62b7aa.chunk.css"
+  },
+  {
+    "revision": "7d745084a73b35ead744",
+    "url": "./static/js/2.94f2702d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.94f2702d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7686073839be7166df5e",
+    "url": "./static/js/main.7121b629.chunk.js"
+  },
+  {
+    "revision": "cb7eb6f828552bf2bc55",
+    "url": "./static/js/runtime-main.8b32b252.js"
+  }
+]);
